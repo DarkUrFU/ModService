ee807f2f24fdca929c0e0b2978b04e3a75f16643
SAlexiy <serovalexey@outlook.com>
2023-10-06 23:30:51 +0500

    initial

---
+++ src/main/java/com/darkurfu/modservice/ModServiceApplication.java
@@ -0,0 +1,13 @@
+package com.darkurfu.modservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/darkurfu/modservice/ModServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.darkurfu.modservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
